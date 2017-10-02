8b41f551db715ffdb0a8f283ef94cbfac8815c03
FromDerik <DerikMalcolm@gmail.com>
2017-10-02 09:35:47 -0500

    Added the navigation bar items

--- Shared Source/Controller/ComposeController.swift
+++ Shared Source/Controller/ComposeController.swift
@@ -13,12 +13,29 @@ class ComposeController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancelButton))
+        let composeButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(handleComposeButton))
+        
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = cancelButton
+        navigationItem.rightBarButtonItem = composeButton
+        
         setupViews()
     }
     
     func setupViews() {
         
+    }
+    
+    @objc func handleComposeButton() {
+        
+    }
+    
+    @objc func handleCancelButton() {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
