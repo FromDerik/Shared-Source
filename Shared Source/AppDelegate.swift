c76c0a73cd34b221cd291f5b4c45939df6399bf1
FromDerik <DerikMalcolm@gmail.com>
2017-10-06 12:30:23 -0500

    Keeping headers at top of screen

--- Shared Source/AppDelegate.swift
+++ Shared Source/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         let layout = UICollectionViewFlowLayout()
-//        layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
+        layout.sectionHeadersPinToVisibleBounds = true
         
         let homeController = HomeController(collectionViewLayout: layout)
         
