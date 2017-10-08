d910c6627c23cacbce90da1497f9a5beded3bac7
FromDerik <DerikMalcolm@gmail.com>
2017-10-08 03:43:39 -0500

    search

--- Shared Source/Controller/SearchController.swift
+++ Shared Source/Controller/SearchController.swift
@@ -15,15 +15,20 @@ class SearchController: UITableViewController {
     let headerId = "headerId"
     
     var users = [Users]()
+    var filteredUsers = [Users]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .lighterBlue
         tableView.separatorColor = .darkerBlue
         tableView.separatorInset.left = 0
-        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: headerId)
+        
+        let searchController = UISearchController()
+    	searchController.searchResultsUpdater = self
+		searchController.hidesNavigationBarDuringPresentation = false
+		searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
         
         navigationController?.navigationBar.barTintColor = .navBlue
         navigationController?.navigationBar.tintColor = .white
@@ -47,27 +52,40 @@ class SearchController: UITableViewController {
         }, withCancel: nil)
     }
     
+    func updateSearchResults(for searchController: UISearchController) {
+	    if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+	        filteredUsers = users.filter { user in
+	            return user.lowercased().contains(searchText.lowercased())
+	        }
+	        
+	    } else {
+	        filteredUsers = users
+	    }
+	    tableView.reloadData()
+	}
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        let user = users[indexPath.row]
-        cell.backgroundColor = .lighterBlue
-        cell.textLabel?.text = user.username
-        cell.textLabel?.textColor = .white
+        if let searchUsers = filteredUsers {
+        	let user = searchUsers[indexPath.row]
+	        cell.backgroundColor = .lighterBlue
+	        cell.textLabel?.text = searchUsers.username
+	        cell.textLabel?.textColor = .white
+        }
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let searchUsers = filteredUsers else {
+        	return 0
+        }
         
-        return users.count
+        return searchUsers.count
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerId)
-        return view
     }
 
 }
