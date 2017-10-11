a503cfb7e3e87b27d234646b44e78511e10b7ddc
DerikMalcolm <DerikMalcolm@gmail.com>
2017-10-11 13:59:08 -0500

    Added InputBar

---
+++ Shared Source/Views/InputBar.swift
@@ -0,0 +1,22 @@
+//
+//  InputBar.swift
+//  Shared Source
+//
+//  Created by Derik Malcolm on 10/11/17.
+//  Copyright © 2017 Derik Malcolm. All rights reserved.
+//
+
+import UIKit
+
+class InputBar: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
